e7ae02b653ed80cbcd632e33088fea88eef1c855
GLOBAL\I306365 <stonna.wang@sap.com>
2018-09-05 13:07:54 +0800

    Add test case.

--- config/setupTests.js
+++ config/setupTests.js
@@ -1,6 +1,5 @@
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import 'jsdom-global/register';
 
 Enzyme.configure({ adapter: new Adapter() });
 console.log("set up start.");
--- src/components/InvitingPad.test.js
+++ src/components/InvitingPad.test.js
@@ -3,15 +3,11 @@ import InvitingPad from "./InvitingPad";
 
 describe("InvitingPad test", () => {
 	beforeAll(() => {
-		// global.window = {
-		// 	addEventListener: () => {}
-		// };
 		global.wrapper = shallow(<InvitingPad />);
 	});
 
 	afterAll(() => {
 		global.wrapper = null;
-		// global.window = null;
 	});
 
 	it ("InvitingPad mount",() => {
@@ -20,7 +16,56 @@ describe("InvitingPad test", () => {
 
 	it ("InvitingPad popup InvitingInfo", () => {
 		wrapper.instance().inviteBtnHandler();
-		wrapper.update();
-		expect(wrapper.find(".info-list").length).toEqual(1);
+		setTimeout(() => {
+			wrapper.update();
+			expect(wrapper.find(".modal").length).toEqual(1);
+			expect(wrapper.find(".info-list").length).toEqual(1);
+			let currState = Object.assign({}, wrapper.instance().state);
+			let invitationInfo = Object.assign({}, currState.invitationInfo);
+			invitationInfo.fullName = "";
+			invitationInfo.email = "hello@sa.com";
+			invitationInfo.emailAgain = "hello@sa.com";
+			currState.invitationInfo = invitationInfo;
+			wrapper.instance().setState(currState);
+			setTimeout(() => {
+				wrapper.update();
+				wrapper.instance().inviteSubmitHandler({
+					preventDefault: () => {}
+				});
+				setTimeout(() => {
+					wrapper.update();
+					expect(wrapper.find(".error-input").length).toEqual(1);
+					invitationInfo.fullName = "aaa";
+					invitationInfo.email = "hello@sb.com";
+					invitationInfo.emailAgain = "hello@sa.com";
+					currState.invitationInfo = invitationInfo;
+					wrapper.instance().setState(currState);
+					setTimeout(() => {
+						wrapper.update();
+						expect(wrapper.find(".error-input").length).toEqual(2);
+					}, 300);
+				}, 300);
+			}, 300);
+		}, 300);
+	});
+
+	it ("Handler which will be called after invited successfully", () => {
+		wrapper.instance().handleAfterInvitation({});
+		setTimeout(() => {
+			wrapper.update();
+			expect(wrapper.contains("You will be one of first to experience Broccoli")).toEqual(true);
+		}, 500);
+	});
+
+	it ("Handler which will be called after invited failed", () => {
+		wrapper.instance().handleRejectedInvitation({
+			entity: {
+				errorMessage: "error!"
+			}
+		});
+		setTimeout(() => {
+			wrapper.update();
+			expect(wrapper.contains("error!")).toEqual(true);
+		}, 500);
 	});
 });
